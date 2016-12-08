b0c1e4f418bebce30eed114654e2b4c6a588e3d2
zhengjinwei123 <2538698032@qq.com>
2016-12-08 14:41:40 +0800

    整理代码

--- mnode/mnode/app.js
+++ mnode/mnode/app.js
@@ -2,11 +2,11 @@
  * Created by zhengjinwei on 2016/11/23.
  */
 var Path = require('path');
-var Pomelo = require('pomelo-logger');
-Pomelo.configure(Path.join(__dirname, './config/logger.json'));
-var Logger = Pomelo.getLogger('jadeLoader', __filename, process.pid);
 var JadeLoader = require("./plugin/app").JadeLoader;
 var HttpServer = require("./plugin/app").HttpServer;
+var LogUtil = require("./utils/app").Logger;
+var Singleton = require("./utils/app").Singleton;
+var logger = Singleton.getDemon(LogUtil, Path.join(__dirname, "./config/logger.json"), Path.join(__dirname, "./logs"));
 
 JadeLoader.init(Path.join(__dirname, "./"), true, 60, function () {
     console.log("jadeLoader success");
@@ -19,25 +19,25 @@ JadeLoader.init(Path.join(__dirname, "./"), true, 60, function () {
     }, Path.join(__dirname, "../httpServerTest"));
 
     httpS.on("ready", function (log) {
-        console.log(log);
+        logger.info("Http",log);
     });
     httpS.on("error", function (log) {
-        console.log(log);
+        logger.info("Http",log);
     });
     httpS.on("connect-connect", function (log) {
-        console.log(log);
+        logger.info("Http",log);
     });
     httpS.on("connect-error", function (log) {
-        console.log(log);
+        logger.info("Http",log);
     });
     httpS.on("connect-disconnect", function (log) {
-        console.log(log);
+        logger.info("Http",log);
     });
     httpS.on("connect-response", function (log) {
-        console.log(log);
+        logger.info("Http",log);
     });
     httpS.on("connect-errorcode", function (log) {
-        console.log(log);
+        logger.info("Http",log);
     });
 
     httpS.createServer();
@@ -50,15 +50,4 @@ JadeLoader.on("hotLoad", function (resp) {
     console.log(resp);
 });
 
-
-
-/**************httpServer 测试********************/
-
-//test logutil
-//var LogUtil = require("./utils/app").Logger;
-//var Singleton = require("./utils/app").Singleton;
-//
-//var logger = Singleton.getDemon(LogUtil, Path.join(__dirname, "./config/logger.json"), Path.join(__dirname, "./logs"));
-//
-//logger.info("Http", "hallore", 1, 2, true, [1, 2, 3, 4], {a: 1});
 
