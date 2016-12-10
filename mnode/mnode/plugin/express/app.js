bb8fd6e3dbd855a5a04d512c4ce0f7a52699911d
zhengjinwei123 <2538698032@qq.com>
2016-12-10 17:06:39 +0800

    update express

--- mnode/mnode/plugin/express/app.js
+++ mnode/mnode/plugin/express/app.js
@@ -12,9 +12,13 @@ var MorganLogger = require('morgan'),
     Session = require('express-session');
 var ObjUtil = require("../../utils/app").Object;
 var FileUtil = require("../../utils/app").File;
+var EventEmitter = require("events").EventEmitter;
+var Util = require("util");
 
 
 var ExpressPlugin = function (host, port, path) {
+    EventEmitter.call(this);
+
     var argsCnt = ObjUtil.count(arguments);
     if (argsCnt < 2) {
         if (argsCnt == 0) {
@@ -48,60 +52,67 @@ var ExpressPlugin = function (host, port, path) {
     if (!FileUtil.isDirectory(path)) {
         throw new Error(path + " must be valid directory")
     }
-
-
+    
     this.app = Express();
 };
+Util.inherits(ExpressPlugin,EventEmitter);
 
 ExpressPlugin.prototype.start = function () {
-    this.app.set('views', Path.join(this.path, 'views'));
-    this.app.set('view engine', "ejs");
-    this.app.use(MorganLogger('dev'));
-    this.app.use(BodyParser.json());
-    this.app.use(BodyParser.urlencoded({extended: false}));
-    this.app.use(CookieParser());
-
-
-    this.app.use(Session({
-        secret: 'express-secret',
-        cookie: {maxAge: 1000 * 60 * 60 * 24 * 30}, // 30 days
-        name: 'expressSecret'
-    }));
-
-    this.app.use(Express.static(Path.join(this.path, 'public')));
-    this.app.use(Express.static(Path.join(this.path, 'staticFile')));
-
-    this.app.use(function (req, res, next) {
-        var url = req.originalUrl;
-        if (url == '/login') {
-            if (req.session && req.session.user) {
-                return res.redirect("/index");
-            } else {
-                return next(null);
-            }
-        }
-        if (!req.session || !req.session.user) {
-
-            if (url.indexOf("checksum_client") != -1 || url.indexOf("checksum_client") != -1) {
-                next(null);
-            } else {
-                res.redirect("/login");
-            }
-
-        } else {
-            next();
-        }
+    var self = this;
+    Http.createServer(this.app).listen(this.port, function () {
+        self.emit('ready');
     });
 
-    this.app.use(function (req, res) {
-        res.statusCode = 404;
-        res.end();
-    });
+    self.on('ready',function(){
+        self.app.set('views', Path.join(this.path, 'views'));
+        self.app.set('view engine', "ejs");
+        self.app.use(MorganLogger('dev'));
+        self.app.use(BodyParser.json());
+        self.app.use(BodyParser.urlencoded({extended: false}));
+        self.app.use(CookieParser());
 
-    if (this.app.get('env') == 'development') {
-        process.on('uncaughtException', function (err) {
-            console.error(' Caught exception: ', err.stack, ' error: ', err);
-            process.exit(1);
+
+        self.app.use(Session({
+            secret: 'express-secret',
+            cookie: {maxAge: 1000 * 60 * 60 * 24 * 30}, // 30 days
+            name: 'expressSecret'
+        }));
+
+        self.app.use(Express.static(Path.join(this.path, 'public')));
+        self.app.use(Express.static(Path.join(this.path, 'staticFile')));
+
+        self.app.use(function (req, res, next) {
+            var url = req.originalUrl;
+            if (url == '/login') {
+                if (req.session && req.session.user) {
+                    return res.redirect("/index");
+                } else {
+                    return next(null);
+                }
+            }
+            if (!req.session || !req.session.user) {
+
+                if (url.indexOf("checksum_client") != -1 || url.indexOf("checksum_client") != -1) {
+                    next(null);
+                } else {
+                    res.redirect("/login");
+                }
+
+            } else {
+                next();
+            }
         });
-    }
+
+        self.app.use(function (req, res) {
+            res.statusCode = 404;
+            res.end();
+        });
+
+        if (self.app.get('env') == 'development') {
+            process.on('uncaughtException', function (err) {
+                console.error(' Caught exception: ', err.stack, ' error: ', err);
+                process.exit(1);
+            });
+        }
+    });
 };
