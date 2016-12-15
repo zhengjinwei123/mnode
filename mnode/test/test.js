809715dfefb088c712e29a606e5cfaafb762a5f8
zhengjinwei123 <2538698032@qq.com>
2016-12-15 22:10:42 +0800

    update

--- mnode/test/test.js
+++ mnode/test/test.js
@@ -3,11 +3,12 @@
  */
 
 var Async = require("async");
-Async.each({a:1,b:2},function(o,cb){
+Async.each({a: 1, b: 2}, function (o, cb) {
     console.log(o);
     cb(null);
-},function(err,resp){
+}, function (err, resp) {
 
 });
 
 console.log(typeof {})
+
