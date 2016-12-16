9b833015f6eaf632f56b80505bb54d7d67c55bcb
zhengjinwei123 <2538698032@qq.com>
2016-12-16 18:26:56 +0800

    去掉调试代码

--- mnode/mnode/plugin/process/worker.js
+++ mnode/mnode/plugin/process/worker.js
@@ -6,8 +6,6 @@ var SyncTask = require("./SyncTask");
 var HttpServer = function (workerid) {
     SyncTask.call(this, workerid);
 
-    //this.workerid = workerid;
-
     var self = this;
     http.Server(function (req, res) {
         res.writeHead(200);
@@ -15,8 +13,6 @@ var HttpServer = function (workerid) {
         self.sendTask('calcV', 1, 2, 3,function(data){
             res.end("调用进程返回：" + data);
         });
-
-        //process.send("Process " + process.pid + " handled request");
     }).listen(8089, '127.0.0.1', function () {
         console.log("Child Server Running on Process: " + process.pid);
     });
