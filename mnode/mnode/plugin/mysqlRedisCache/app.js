fab93108fbcc968c48cb6ce3773ba761fe637796
zhengjinwei123 <2538698032@qq.com>
2017-01-16 19:01:56 +0800

    update

--- mnode/mnode/plugin/mysqlRedisCache/app.js
+++ mnode/mnode/plugin/mysqlRedisCache/app.js
@@ -4,8 +4,22 @@
  * 解决操作mysql的复杂度和速率，利用redis作为缓存，redis定时落地到mysql
  */
 var XmlParser = require("../../utils/xml-parser/app");
+var FileUtil = require("../../utils/file-utils/app");
 
 var mysqlRedisCache = function (xmlPath) {
-    
+    if (FileUtil.isFile(xmlPath)) {
+        throw new Error(xmlPath + " file not exits");
+    }
+    this.xmlPath = xmlPath;
+
+    this.genDBCode();
 };
+
+mysqlRedisCache.prototype.genDBCode = function () {
+    XmlParser(this.xmlPath, function (err, results) {
+        
+    });
+};
+
+
 
