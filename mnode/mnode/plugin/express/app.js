6e950fc4ee8613f35538ee78cebb7550ffac483f
zhengjinwei123 <2538698032@qq.com>
2017-01-16 12:44:36 +0800

    express req内容过大调整

--- mnode/mnode/plugin/express/app.js
+++ mnode/mnode/plugin/express/app.js
@@ -161,8 +161,8 @@ ExpressPlugin.prototype.start = function (callback) {
         self.app.set('views', Path.join(self.path, 'views'));
         self.app.set('view engine', "ejs");
         self.app.use(MorganLogger('dev'));
-        self.app.use(BodyParser.json());
-        self.app.use(BodyParser.urlencoded({extended: false}));
+        self.app.use(BodyParser.json({limit:'50mb'}));
+        self.app.use(BodyParser.urlencoded({limit:"50mb",extended: false}));
         self.app.use(CookieParser());
 
         var iconPath = self.path + '/public/images/favicon.ico';
