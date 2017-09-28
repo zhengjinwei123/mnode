6c33d5ad16a29945a70da73ae538a9b803e96d82
zhengjinwei123 <2538698032@qq.com>
2017-09-28 11:34:55 +0800

    fix syncTask bugs

--- mnode/mnode/plugin/http/SyncTask.js
+++ mnode/mnode/plugin/http/SyncTask.js
@@ -5,21 +5,34 @@ var _ = require("lodash");
 var SyncTask = function (workerid) {
     EventEmitter.call(this);
     this.workerid = workerid;
+    this.listener = null;
+    this.state = false;
 };
 
 Util.inherits(SyncTask, EventEmitter);
 
 SyncTask.prototype.sendTask = function (msgID, data, callback) {
+    this.state = true;
     process.send({
         msgid: msgID,
         workerid: this.workerid,
         data: data
     });
 
+    this.listener = callback;
+
+    var self = this;
     this.on("return", function (data) {
+        //self.removeAllListeners();
+        self.removeListener("return", self.listener);
+        self.state = false;
         callback(data);
     });
-
 };
+
+SyncTask.prototype.taskEnable = function () {
+    return (this.state == false);
+};
+
 
 module.exports = SyncTask;
